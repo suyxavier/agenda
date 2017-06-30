85118ccace3211e5deaa45a9d124912ad5aa84f6
suyxavier <suy.xavier@gmail.com>
2017-06-30 11:56:19 -0300

    Add files via upload

--- controlador_agenda.php
+++ controlador_agenda.php
@@ -1,40 +1,86 @@
-<?php
+<?php //controlador agenda
+
 
     function cadastrar(){
 
-        $contatos = file_get_contents("contatos.json", true);
-        $contatos = json_decode($contatos, true);
+    $contatos = file_get_contents("contatos.json", true); //guardando os resultados
+    $contatos = json_decode($contatos, true); // convertendo para um array
+    $contato = [
+        "id"       => uniqid(), //gerar um id novo e diferente de todos cada vez que atualizar
+        "nome"     => $_POST['nome'],
+        "email"    => $_POST['email'],
+        "telefone" => $_POST['telefone']
+    ];
 
+    array_push($contatos, $contato);
+    $dados_json = json_encode($contatos, JSON_PRETTY_PRINT); //arrumar na hora de executar
 
-        $contato = [
-            "id"        => uniqid (),
-            "nome"      => $_POST['nome'],
-            "email"     => $_POST['email'],
-            "telefone"  => $_POST['telefone']
+    file_put_contents("contatos.json", $dados_json);
+    header('Location: index.php');
+}
 
-        ];
-
-        array_push($contatos, $contato);
-
-        $dados_jason = json_encode($contatos, JSON_PRETTY_PRINT);
-
-        file_put_contents("contatos.json", $dados_jason);
-
-        header('Location: index.php');
-
-    } //fim cadastrar()
-
-    function pegarContatos(){
-
-        $contatos = file_get_contents("contatos.json", true);
-        $contatos = json_decode($contatos, true);
-
+    function pegarContatos()
+    {
+        $contatos = file_get_contents("contatos.json", true); //guardando os resultados
+        $contatos = json_decode($contatos, true); // convertendo para um array
         return $contatos;
+    }
+
+
+function editarContato($valorBuscado){
+
+
+        $contatos = file_get_contents("contatos.json", true);
+        $contatos = json_decode($contatos, true);
+
+        foreach ($contatos as $contato) {
+
+            if ($contato ['nome'] == $valorBuscado) {
+
+
+                echo $contato ['nome'];
+                echo "\n";
+                echo $contato['email'];
+                sleep(1);
+
+
+            }
+
+        }
 
     }
 
-//GERENCIAMENTO DE ROTAS
 
-    if($_GET['acao']== 'cadastrar'){
-        cadastrar();
+
+
+    function excluirContato($idContato)
+    {
+
+        $contatos = file_get_contents("contatos.json, true"); //guardando os resultados
+        $contatos = json_decode($contatos, true); // convertendo para um array
+
+        foreach ($contatos as $posicao => $contato) {
+
+            if ($contato ['nome'] == $idContato) {
+                unset($contatos[$posicao]);
+                break;
+
+            }
+
+        }
+
+        $contatos = json_decode($contatos, JSON_PRETTY_PRINT);
+        file_put_contents("contatos.json", $contatos);
+
+        header('Location:index.php');
     }
+
+
+
+    // Geranciamento de rotas
+//    if ($_GET['acao'] == 'cadastrar'){
+//        cadastrar();
+//    }elseif ($_GET['acao']== 'excluir'){
+//        excluirContato($_GET['id']);
+//    }
+
